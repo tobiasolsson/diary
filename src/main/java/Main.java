e373724f9fd98b8e7c6169789476c4f8173adbee
Tobias Olsson <tobias.olsson@pm.me>
2022-10-05 13:56:26 +0200

    Able to add to json file, without overwriting old entries

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import model.Post;
 import model.User;
 
 import java.io.IOException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -16,19 +17,16 @@ public class Main {
     // TODO 2. Read from JSON file the entries
     // TODO 3. Add user JSON and let user create new User
     // TODO 4. Let user log in and only then can you read (only that users entries) and write
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
 
         // Test users
         List<User> users = new ArrayList<>();
         users.add(new User("Tobias"));
 
-        // Test posts
-        // TODO Format long strings on display
-        List<Post> posts = new ArrayList<>();
-        posts.add(new Post("test1", "test1", LocalDate.now().toString()));
-        posts.add(new Post("test2", "test2", LocalDate.now().toString()));
-        posts.add(new Post("Lorem ipsum dolor sit amet", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At erat pellentesque adipiscing commodo elit at. Non nisi est sit amet. Fermentum posuere urna nec tincidunt. A iaculis at erat pellentesque adipiscing commodo elit. In vitae turpis massa sed elementum. Nulla pharetra diam sit amet nisl. Molestie at elementum eu facilisis sed odio morbi quis commodo. Fringilla ut morbi tincidunt augue interdum velit. Lorem ipsum dolor sit amet consectetur adipiscing elit. Blandit massa enim nec dui nunc. Quisque sagittis purus sit amet volutpat.", LocalDate.now().toString()));
+
+        // TODO Deal with empty file, try/catch?
+        List<Post> posts = updatePostsList();
 
         while (true) {
             Menu.displayStartMenu();
@@ -51,7 +49,9 @@ public class Main {
 
                     Post newEntry = new Post(postTitle, postBody, currentDate);
 
-                    addNewEntry(newEntry);
+                    // Update posts List<> to current values in JSON file
+                    // returns new list of posts, so we can update posts list
+                    posts = addNewEntry(newEntry);
 
                 } else if (choiceOne == 3) {
                     break;
@@ -62,10 +62,26 @@ public class Main {
         }
     }
 
-    static void addNewEntry(Post newEntry) throws IOException {
+    // TODO Put in own Class? service/utility
+    static List<Post> updatePostsList() throws IOException {
         ObjectMapper mapper = new ObjectMapper();
 
+        return List.of(mapper.readValue(Paths.get("src/main/resources/diary.json").toFile(),
+                Post[].class));
+    }
 
-        mapper.writeValue(Paths.get("src/main/resources/diary.json").toFile(), newEntry);
+    static List<Post> addNewEntry(Post newEntry) throws IOException {
+        // Update list of posts and write to JSON file, then return the new list to update main function
+        ObjectMapper mapper = new ObjectMapper();
+        Path path = Paths.get("src/main/resources/diary.json");
+
+        // TODO Break out to own function when users are involved
+        List<Post> updatedPosts = new ArrayList<>(updatePostsList());
+        updatedPosts.add(newEntry);
+
+        mapper.writeValue(path.toFile(), updatedPosts);
+
+
+        return updatedPosts;
     }
 }
